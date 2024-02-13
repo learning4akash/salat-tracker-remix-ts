c6143375b6568d40822e6be789b29166b77338ea
Ashraful Islam <theakashsarkar7@gmail.com>
2024-02-13 08:30:14 +0600

    index file update

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,8 @@
-import type { MetaFunction } from "@remix-run/node";
-
-export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
-};
-
-export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+import { LoaderFunctionArgs, redirect } from "@remix-run/node";
+export async function loader({ request }: LoaderFunctionArgs) {
+    const getCookie: string | null  = request.headers.get('Cookie');
+    if (getCookie) {
+        return redirect("/tracker");
+    }
+    return redirect("/setting");
 }
